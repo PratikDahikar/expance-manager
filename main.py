08977de0b6e95abc9e7621aedb6e851c3df9bf62
Pratik Dahikar <pratikdahikar22@gamil.com>
2024-08-17 22:44:49 +0530

    updated

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import toml
 # Load the TOML file
 # config = toml.load(st.secrets["google_sheets"])
 
+print('loading google creds')
 # Extract credentials from the TOML file
 # google_sheets_creds = config['google_sheets']
 google_sheets_creds = st.secrets["google_sheets"]
+
+print('loading google creds complete')
 
 # Setup credentials for gspread
 creds = ServiceAccountCredentials.from_json_keyfile_dict(google_sheets_creds, [
