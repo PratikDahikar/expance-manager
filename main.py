975fea86d572a4ae987260340231c7fee2298520
Pratik Dahikar <pratikdahikar22@gamil.com>
2024-08-17 15:05:53 +0530

    main file updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import gspread
+# import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import toml
@@ -16,17 +16,17 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(google_sheets_creds, [
     "https://www.googleapis.com/auth/drive"
 ])
 
-client = gspread.authorize(creds)
+# client = gspread.authorize(creds)
 
 # Open the Google Sheet
-sheet = client.open("expance-manager").sheet1  # or specify the sheet name
+# sheet = client.open("expance-manager").sheet1  # or specify the sheet name
 
 # Access the Google Sheet
 # sheet = client.open("sheet1").worksheet("expance-manager")
 
-data = sheet.get_all_records() 
+# data = sheet.get_all_records() 
 # print(data)
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 
-st.write(df)
+# st.write(df)
